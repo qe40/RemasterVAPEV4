054e0ca68909a24c60d0d437bd7feab6f8da3955
qe40 <jyroke4@gmail.com>
2025-09-16 23:42:31 -0700

    Update 6872265039.lua

--- games/6872265039.lua
+++ games/6872265039.lua
@@ -6,7 +6,7 @@ local replicatedStorage = cloneref(game:GetService('ReplicatedStorage'))
 local inputService = cloneref(game:GetService('UserInputService'))
 
 local lplr = playersService.LocalPlayer
-local vape = shared.vape
+local vape = shared.NewVape
 local entitylib = vape.Libraries.entity
 local sessioninfo = vape.Libraries.sessioninfo
 local bedwars = {}
