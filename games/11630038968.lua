5c8dd43857bd8c959a6ed684268f2a3b8289a396
qe40 <jyroke4@gmail.com>
2025-09-16 23:32:53 -0700

    Update 11630038968.lua

--- games/11630038968.lua
+++ games/11630038968.lua
@@ -9,7 +9,7 @@ local runService = cloneref(game:GetService('RunService'))
 
 local gameCamera = workspace.CurrentCamera
 local lplr = playersService.LocalPlayer
-local vape = shared.vape
+local vape = shared.NewVape
 local entitylib = vape.Libraries.entity
 local targetinfo = vape.Libraries.targetinfo
 local prediction = vape.Libraries.prediction
